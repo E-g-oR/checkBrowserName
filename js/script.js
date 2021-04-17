c05dc69f47cdf638d59cc022ac514a5f9e22fcde
E-g-oR <emuzych@gmail.com>
2021-04-17 09:26:18 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -5,5 +5,5 @@ const searchPartiPhone = "iPhone";
 const insult = "бомжафон";
 browserNameElem.textContent = browserName;
 if (browserName.includes(searchPartXiaoMi)) {
-   alert("У тебя... ".insult);
+   alert("У тебя... " + insult);
 } else if (browserName.includes(searchPartiPhone)) { alert("Ты крут!") } else alert("Норм устройство")
