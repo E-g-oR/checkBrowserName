632187ee4edafe65f0a10b5ac43cdd5276a16a8f
E-g-oR <emuzych@gmail.com>
2021-04-17 09:22:08 +0300

    new alerts

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 const browserNameElem = document.querySelector('.browser-name');
 let browserName = navigator.userAgent;
+const searchPartXiaoMi = "XiaoMi";
+const searchPartiPhone = "iPhone";
+const insult = "бомжафон";
 browserNameElem.textContent = browserName;
+if (browserName.includes(searchPartXiaoMi)) {
+   alert("У тебя... ".insult);
+} else if (browserName.includes(searchPartiPhone)) { alert("Ты крут!") } else alert("Норм устройство")
